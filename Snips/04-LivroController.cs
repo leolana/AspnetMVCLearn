a133655991d095a377741c79fdb76006b92bb299
Leonardo Lucas Lana <leolana.lucas@hotmail.com>
2016-06-30 18:34:28 -0300

    Adicionado a apresentação do dia de Views

---
+++ Snips/04-LivroController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class LivroController : Controller
+    {
+        public ActionResult BuscarAutor(string termo)
+        {
+            var autores = db.Livroes.Where(l => l.Autor.Contains(termo))
+                                    .Select(l => l.Autor)
+                                    .ToList();
+
+            return PartialView("_BuscarAutorPartialView", autores);
+        }
+    }
+}
---
+++ Snips/13-Livro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Livro
+    {
+        [DataType(DataType.Date)]
+        [Required]
+        [DisplayName("Data de lançamento")]
+        [DisplayFormat(DataFormatString="{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)] //Formatação para exibir apenas Data
+        [UIHint("Datepicker")]
+        public DateTime DataLancamento { get; set; }
+    }
+}
