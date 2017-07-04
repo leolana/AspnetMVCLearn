2ac63d91088d8e021011c6cbd71940a49ca09838
Leonardo Lana <leonardo.lana@catho.com>
2017-07-04 19:29:58 -0300

    Separado os snips

---
+++ Snips/5-EF/01-HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            var livros = db.Livroes.Take(6).ToList();
+            return View(livros);
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
