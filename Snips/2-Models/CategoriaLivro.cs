c9a0048d1a8e610bdd159baff55408f65a55ea57
Natanael Lemos <Natanael Lemos>
2017-07-04 16:42:34 -0300

    Snippets de models

---
+++ Snips/2-Models/CategoriaLivro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace BookStore.Models
+{
+    public class CategoriaLivro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        public virtual ICollection<Livro> Livros { get; set; }
+    }
+}
---
+++ Snips/2-Models/Compra.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Compra
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Cliente")]
+        [ForeignKey("Cliente")]
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+
+        [DisplayName("Valor Total")]
+        public decimal ValorTotal { get; set; }
+
+        [DisplayName("Valor do Frete")]
+        public decimal ValorFrete { get; set; }
+    }
+}
